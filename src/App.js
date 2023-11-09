0ce66ae925a85f5440474f192980e69d56d46d2f
Ghaalia <Ghaliaals9@gmail.com>
2023-11-09 17:55:49 +0300

    NavBar

--- src/App.js
+++ src/App.js
@@ -4,15 +4,20 @@ import HomePage from "./Pages/HomePage";
 import Welcome from "./Pages/Welcome";
 import Register from "./Pages/Register";
 import Login from "./Pages/Login";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" Component={Welcome} />
-      <Route path="/Register" Component={Register} />
-      <Route path="/Login" Component={Login} />
-      <Route path="/HomePage" Component={HomePage} />
-    </Routes>
+    <div>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" Component={Welcome} />
+        <Route path="/Register" Component={Register} />
+        <Route path="/Login" Component={Login} />
+        <Route path="/HomePage" Component={HomePage} />
+      </Routes>
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,8 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  daisyui: {
+    themes: ["cupcake"],
+  },
+  plugins: [require("daisyui")],
 };
