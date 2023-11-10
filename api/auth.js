124ba4b1063cdda1e283c2f2d086b43ff9440294
sheikhaald <smaldeai98@gmail.com>
2023-11-10 20:09:20 +0300

    pudh

---
+++ api/auth.js
@@ -0,0 +1,17 @@
+import instance from ".";
+
+const Login = async (userInfo) => {
+  const res = await instance.post("/", userInfo);
+  return res.data;
+};
+
+const Register = async (userInfo) => {
+  const formData = new FormData();
+  for (let key in userInfo) {
+    formData.append(key, userInfo[key]);
+  }
+  const res = await instance.post("", formData);
+  return res.data;
+};
+
+export { Login, Register };
