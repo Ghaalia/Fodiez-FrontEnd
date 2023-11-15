f6260391cbf015a94770af8a406eef93f4a50f1b
Ghaalia <Ghaliaals9@gmail.com>
2023-11-15 17:48:07 +0300

    Last Version

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -2,12 +2,11 @@ import React from "react";
 import Navbar from "../Components/Navbar";
 import AddButton from "../Components/AddButton";
 import CategoryList from "../Components/CategoryList";
-import RecipeModal from "../Components/RecipeModal";
 
 const HomePage = () => {
   return (
     <div className="">
-      <RecipeModal />
+      <CategoryList />
     </div>
   );
 };
