6c94f8cc8c374699d54f70c87f9d57df2e0dc0d6
Ghaalia <Ghaliaals9@gmail.com>
2023-11-15 18:21:36 +0300

    Home Page Design

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,13 +1,28 @@
 import React from "react";
-import Navbar from "../Components/Navbar";
-import AddButton from "../Components/AddButton";
 import CategoryList from "../Components/CategoryList";
-import RecipeModal from "../Components/RecipeModal";
 
 const HomePage = () => {
   return (
-    <div className="">
-      <RecipeModal />
+    <div className="w-screen flex justify-center items-center">
+      <div className="flex w-[95%] h-screen justify-between align-middle items-center ">
+        <div className="fixed">
+          <h2 className="text-5xl font-serif font-medium underline underline-offset-5 decoration-blue-300 ">
+            Discover a world
+          </h2>
+
+          <h2 className="text-5xl font-serif font-medium underline underline-offset-5 decoration-blue-300 ">
+            of flavors right at
+          </h2>
+
+          <h2 className="text-5xl font-serif font-medium underline underline-offset-5 decoration-blue-300 ">
+            your fingertips.
+          </h2>
+        </div>
+
+        <div className="w-96"></div>
+
+        <CategoryList />
+      </div>
     </div>
   );
 };
